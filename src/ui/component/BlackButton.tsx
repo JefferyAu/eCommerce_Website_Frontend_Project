4e894a435478c43712322feef5577d00bdef28b7
JefferyAu <jefferyauweb@gmail.com>
2024-09-05 13:28:39 +0800

    test CI/CD

--- src/ui/component/BlackButton.tsx
+++ src/ui/component/BlackButton.tsx
@@ -3,7 +3,6 @@ import {createTheme, ThemeProvider} from "@mui/material/styles";
 export default function BlackButton(){
   const blackTheme = createTheme({
     palette: {
-      mode: 'dark',
       primary: {
         main: '#000000', // 黑色主色調
       },
